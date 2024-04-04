3bf24c916ee12ddb3703fd0314d3fdc1daba7b8b
luan boaventura <ultrasenha@gmail.com>
2024-04-04 08:00:50 -0300

    EPA

--- conexao.php
+++ conexao.php
@@ -10,5 +10,12 @@ $conexao = new mysqli($host,$user,$senha,$banco);
  }
  else{
  //   echo 'deu certo a conexao';
+
+  // 1crie um formulario com o bootstrap (index.php)
+    // 2 crie um banco de dados (xamp)
+    // 3 crie uma conexao com o banco (conexao.php)
+    // 4 realizar um cadrasto,inseri o cadrasto no banco de dados (xamp,admin,banco de dados,(nome do banco),cria,ID= INT
+    //NOME=VACHAR,)
+    // 5 cria hash.php
  }
 ?>
