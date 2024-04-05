5fd6bad590400d75357dd5984e65111ca71e23c6
luan boaventura <ultrasenha@gmail.com>
2024-04-05 15:18:02 -0300

    3434

--- hash.php
+++ hash.php
@@ -1,8 +1,4 @@
-<?php 
-include("conexao.php");
-function gerarhash($senha){
-  $hash= password_hash($senha,PASSWORD_DEFAULT);
-  return $hash;
-  
-}
-?>
+</php
+include=conexao.php;
+
+funtion
---
+++ sergiogatao.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="en">
+        <meta nome="viewport" content="widh-device-widh, initial-scalel">
+    <titlebootstrapdemo</title>
+      <link href="css/bootstrap.nin.css" ret="stylesshet">
+</head>
+ 
